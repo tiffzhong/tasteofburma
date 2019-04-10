9ed36432cad12af78b217fcf313f448e0a09267a
Tiffany Zhong <tiffzhong@gmail.com>
2019-04-10 15:40:16 -0700

    nav bar on landing working

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -43,42 +43,37 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing-container">
-        {this.state.isTop ? (
-          <div className="navigation-landing" style={headerTop}>
-            <Link to="/">
-              <img src={logo} height="40" alt="logo" />
-            </Link>
-            <div className="links">
-              <button onClick={this.toggle}>☰</button>
-            </div>
-            <ul className={this.state.toggle ? "show" : "hide"} />
-            <div className="nav-links">
-              <Link to="/">Home</Link>
-              <Link to="/menu">Menu</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-              <a href="tel:6504491378">Call</a>
-            </div>
-          </div>
-        ) : (
-          <div className="navigation-landing" style={headerScroll}>
-            <Link to="/">
-              <img src={logo} height="40" alt="logo" />
-            </Link>
-            <div className="links">
-              <button onClick={this.toggle}>☰</button>
-            </div>
-            <ul className={this.state.toggle ? "show" : "hide"} />
-            <div className="nav-links">
-              <Link to="/">Home</Link>
-              <Link to="/menu">Menu</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-              <a href="tel:6504491378">Call</a>
-            </div>
-          </div>
-        )}
+        <nav style={this.state.isTop ? headerTop : headerScroll}>
+          {this.state.toggle ? (
+            <div className="drawerOpen">
+              <button onClick={this.toggle}>&times;</button>
 
+              <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+              </div>
+            </div>
+          ) : (
+            <div className="drawerHidden">
+              <div>
+                <Link to="/">
+                  <img src={logo} height="40" alt="logo" />
+                </Link>
+              </div>
+              <button onClick={this.toggle}>&#9776;</button>
+
+              <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <a href="tel:6504491378">Call</a>
+              </div>
+            </div>
+          )}
+        </nav>
         <div className="first-view">
           <h1>Taste of Burma</h1> <h6>Welcome!</h6>
         </div>
